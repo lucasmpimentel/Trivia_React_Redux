9fe8cbbb7d3bbd1b2919042b56b2d3133000bce0
calilisantos <calilisantos@gmail.com>
2022-03-18 00:17:59 -0300

    requisito 09. status: finalizado

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { refreshTokenAPI } from '../redux/actions/fetch';
 import { ReactComponent as Loading } from '../image/Eclipse-1s-200px.svg';
+import { setItemRanking } from '../services/syncLocal';
+import { changeScore } from '../redux/actions';
 
-export default class Questions extends Component {
+class Questions extends Component {
   state = {
     questions: [],
     indexQuestions: 0,
     index: 0,
+    timer: 30,
   }
 
   async componentDidMount() {
     const getQuestions = await refreshTokenAPI();
+    console.log(getQuestions);
     this.setState({ questions: (getQuestions.map((item) => {
       const allAnswers = [];
       item.incorrect_answers.forEach((wrongAnswer) => {
@@ -26,6 +32,7 @@ export default class Questions extends Component {
       return ({
         category: item.category,
         question: item.question,
+        difficulty: item.difficulty,
         allAnswers,
       });
     })) }, () => {
@@ -33,6 +40,23 @@ export default class Questions extends Component {
       const randomized = this.randomAnswer(questions[0].allAnswers);
       console.log(randomized);
     });
+  }
+
+  changeScores = (answer) => {
+    const { questions, index, timer } = this.state;
+    const { difficulty } = questions[index];
+    const { player: { name, score, gravatarEmail, assertions }, dispatch } = this.props;
+    const scoreByDifficulty = { hard: 3, medium: 2, easy: 1 };
+    const pontuation = 10;
+    let playerScore = 0;
+    const assertionsCount = assertions + 1;
+    if (answer) {
+      playerScore = score + (pontuation + (timer * scoreByDifficulty[difficulty]));
+      const newPlayerScore = { name, score: playerScore, picture: gravatarEmail };
+      dispatch(changeScore({ score: playerScore, assertions: assertionsCount }));
+      const newPlayerStorage = JSON.stringify(newPlayerScore);
+      setItemRanking(newPlayerStorage);
+    }
   }
 
   handleClick = () => {
@@ -51,7 +75,7 @@ export default class Questions extends Component {
   }
 
   render() {
-    const { handleClick, randomAnswer } = this;
+    const { handleClick, randomAnswer, changeScores } = this;
     const { questions, index } = this.state;
     return (
       <section>
@@ -69,6 +93,7 @@ export default class Questions extends Component {
                         type="button"
                         data-testid={ question
                           .isCorrect ? 'correct-answer' : 'wrong-answer' }
+                        onClick={ () => changeScores(question.isCorrect) }
                       >
                         {question.answerText}
                       </button>
@@ -85,3 +110,16 @@ export default class Questions extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  player: state.player,
+});
+
+Questions.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  player: PropTypes.arrayOf(
+    PropTypes.string,
+  ).isRequired,
+};
+
+export default connect(mapStateToProps)(Questions);
