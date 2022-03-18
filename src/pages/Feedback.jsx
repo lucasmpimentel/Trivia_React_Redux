ffaa17beafd64096e8372ebd5b342ba70135c60e
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-03-18 19:48:02 -0300

    Requisito 13 Finalizado

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { player: { assertions } } = this.props;
+    const FEEDBACK_NUMBER = 3;
     return (
-      <main>
+      <>
         <Header />
-        <h1 data-testid="feedback-text">Feedback</h1>
-      </main>
+        <main>
+          { assertions < FEEDBACK_NUMBER
+            ? <h1 data-testid="feedback-text">Could be better...</h1>
+            : <h1 data-testid="feedback-text">Well Done!</h1> }
+          <div />
+        </main>
+      </>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  player: state.player,
+});
+
+Feedback.propTypes = {
+  player: PropTypes.arrayOf(
+    PropTypes.string,
+  ).isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
